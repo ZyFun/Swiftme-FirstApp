2c7ec5ca41953a86d4cacaf10c8b27234f95e8d6
Dmitriy Danilin <danilindn@icloud.com>
2020-04-25 13:19:16 +0300

    Initial Commit

---
+++ Swiftme-FirstApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swiftme-FirstApp
+//
+//  Created by Дмитрий Данилин on 25.04.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
